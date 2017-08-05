d0646188d6b330f836488022c70d60d024e3f8a0
Antonia Cerda <antoniacerdag@gmail.com>
2017-08-04 20:22:15 -0400

    subiendo todo

---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+	//MENU
+
+	//animacion hover iconos
+	$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+        return this;
+	    }
+	});
+	$(".fa-linkedin").hover(function(){
+		$(".fa-linkedin").animateCss('rotateIn');
+	});
+	$(".fa-github").hover(function(){
+		$(".fa-github").animateCss('rotateIn');
+	});
+	$(".fa-behance").hover(function(){
+		$(".fa-behance").animateCss('rotateIn');
+	});
+
+	$(".devicon-html5-plain-wordmark").hover(function(){
+		$(".devicon-html5-plain-wordmark").animateCss('zoomIn');
+	});
+	$(".devicon-css3-plain-wordmark").hover(function(){
+		$(".devicon-css3-plain-wordmark").animateCss('zoomIn');
+	});
+	$(".devicon-javascript-plain").hover(function(){
+		$(".devicon-javascript-plain").animateCss('zoomIn');
+	});
+	$(".devicon-jquery-plain-wordmark").hover(function(){
+		$(".devicon-jquery-plain-wordmark").animateCss('zoomIn');
+	});
+	$(".devicon-github-plain").hover(function(){
+		$(".devicon-github-plain").animateCss('zoomIn');
+	});
+	$(".devicon-bootstrap-plain-wordmark").hover(function(){
+		$(".devicon-bootstrap-plain-wordmark").animateCss('zoomIn');
+	});
+	$(".devicon-sass-original").hover(function(){
+		$(".devicon-sass-original").animateCss('zoomIn');
+	});
+	$(".devicon-git-plain-wordmark").hover(function(){
+		$(".devicon-git-plain-wordmark").animateCss('zoomIn');
+	});
+	$(".devicon-slack-plain").hover(function(){
+		$(".devicon-slack-plain").animateCss('zoomIn');
+	});
+	$(".devicon-trello-plain").hover(function(){
+		$(".devicon-trello-plain").animateCss('zoomIn');
+	});
+	$(".devicon-sass-original").hover(function(){
+		$(".devicon-sass-original").animateCss('zoomIn');
+	});
+	$(".devicon-trello-plain").hover(function(){
+		$(".devicon-trello-plain").animateCss('zoomIn');
+	});
+	$(".devicon-gulp-plain").hover(function(){
+		$(".devicon-gulp-plain").animateCss('zoomIn');
+	});
+	$(".devicon-photoshop-plain").hover(function(){
+		$(".devicon-photoshop-plain").animateCss('zoomIn');
+	});
+	$(".devicon-illustrator-plain").hover(function(){
+		$(".devicon-illustrator-plain").animateCss('zoomIn');
+	});
+
+	//FUNCION TAGS
+	$("#all").click(function(){
+    	$(".pro-web").show(500);
+    	$(".pro-disenio").show(500);
+	});
+		$("#web").click(function(){
+    	$(".pro-web").show(500);
+    	$(".pro-disenio").hide(500);
+	});
+	$("#graphicdesign").click(function(){
+    	$(".pro-web").hide(500);
+    	$(".pro-disenio").show(500);
+	});
+});
---
+++ assets/js/menu.js
@@ -0,0 +1,31 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function cambionav(){
+
+	var distscroll= window.pageYOffset || document.documentElement.scrollTop;
+	if (distscroll > 150 ){ // al hacer scroll se debe eliminar la clase hide del nav que esta oculto
+		document.getElementById("menu").classList.add('hide');
+
+	}else{
+		document.getElementById("menu").classList.remove('hide');
+	}
+};
+window.addEventListener('scroll' , cambionav);
+
+function navLateral(){
+	var distscroll= window.pageYOffset || document.documentElement.scrollTop;
+	if (distscroll > 450 ){ // al hacer scroll se debe eliminar la clase hide del nav que esta oculto
+		document.getElementById("menu-lateral").classList.remove('hide');
+
+	}else{
+		document.getElementById("menu-lateral").classList.add('hide');
+	}
+}
+
+window.addEventListener('scroll' , navLateral);
